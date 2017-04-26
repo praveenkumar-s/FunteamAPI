aee14ecc9925a50773c4d492169dc55878191a90
praveenkumar-s <praveenkumar.s@realimage.com>
2017-04-26 16:21:05 +0530

    modified binding

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def getuserdata(slackname):
 
 
 if __name__ == '__main__':
-    app.run(debug=False , host='0.0.0.0',port=81)
+    from os import environ
+    app.run(debug=False , host='0.0.0.0', port=environ.get("PORT", 5000))

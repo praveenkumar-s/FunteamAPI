ff6a412925f56ddd6ae60d2f9d06fa8ea9628156
praveenkumar-s <praveenkumar.s@realimage.com>
2018-03-05 10:39:01 +0530

    Fixed issues in greetings

--- slydes_demo_server.py
+++ slydes_demo_server.py
@@ -2,11 +2,11 @@ import requests
 import threading
 import json
 from dateutil.parser import *
-
+import dbservice
 
 def get_latest_data(id):
     latestdate=None
-    url=None
+    url="https://data.justickets.co/assets/cheers_tagline.svg"
     HISTORY_DATA=requests.get(url="https://staging-admin.justickets.co/v2/history/{0}".format(id))
     data= json.loads(HISTORY_DATA.content)
 
@@ -14,12 +14,14 @@ def get_latest_data(id):
         if(items["greeting"] is not None):
             if(latestdate is None):
                 latestdate=parse(items["greeting"][0]["timestamp"])
+                url=items["greeting"][0]["preview"]
             else:
                 if(parse(items["greeting"][0]["timestamp"]) > latestdate):
                     latestdate=parse(items["greeting"][0]["timestamp"])
                     url=items["greeting"][0]["preview"]
     return url
    
+
 
 
 
